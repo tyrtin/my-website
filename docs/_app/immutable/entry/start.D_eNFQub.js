30eabc539491ee75fe2f50e61278d76b6088dbf6
Tyra Tinglev <tyrtin03@elevad.umea.se>
2025-04-23 11:48:31 +0200

    search

--- docs/_app/immutable/entry/start.D_eNFQub.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"../chunks/entry.6s41PKLJ.js";export{a as start};
--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,10 +1,11 @@
 <script>
     let search = ""
     import { goto } from "$app/navigation";
+    import { base } from "$app/paths";
 
 </script>
 
-<form on:submit|preventDefault={()=> goto('/search/'+search)}>
+<form on:submit|preventDefault={()=> goto(base+'/search/'+search)}>
     <input type="text" placeholder="Sök upp en pokemon" bind:value={search} />
   </form>
 
