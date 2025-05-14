a7b10fb0780270a339446ca6a5eec6fb8f7d5a93
Tyra Tinglev <tyrtin03@elevad.umea.se>
2025-05-14 14:24:33 +0200

    message

---
+++ docs/_app/immutable/chunks/update2.BhXhJ_Gi.js
@@ -0,0 +1,21 @@
+import{s as D,n as O}from"./scheduler.Cbj4gE2O.js";import{S as L,i as Q,e as n,s as $,t as s,c as o,k as P,f as U,b as r,g as i,d as a}from"./index.vUVupSp4.js";function j(W){let l,T="Since last time I have started working on the individual character sites. I use fiealdset and legend for for the information box.",u,d,b,h,m,c,f,p,w,y,I,k,v,g,R,B,x,S,_,C,A,q,M;return{c(){l=n("p"),l.textContent=T,u=$(),d=n("br"),b=s(` Today I started by trying to link my "se more" buttons to diffrent sites depending on which character you click on. 
+`),h=n("br"),m=s(` I asked copilot and the AI said to use "$base/charakter" which is a reactive statement. It worked... 
+`),c=n("br"),f=s(` I have also worked on making my site more accesible for everyone. 
+`),p=n("br"),w=s(` I wanted to be able to change my characters outfits by clicking on a button with the name of the outfit on. 
+`),y=n("br"),I=s(` --> So I made a button that was called "warrior outfit" and then a funtion "warrior". And then with help of copilot I figuerd out how to make it possible to change the outfit.
+`),k=n("br"),v=s(` I used a currentOutfit and set it to the img and then in the function I set currentOutfit to a new outfit.
+`),g=n("br"),R=s(` I have made the information easier to read. 
+`),B=n("br"),x=s(` With media quiries I experiment on how to make it better when you use a phone. 
+`),S=n("br"),_=s(`I used "Master Media Queries And Responsive CSS Web Design Like a Chameleon!" youtube video by Slaying the dragon".
+`),C=n("br"),A=s(` --> To get a better understanding of how to use media queries.
+`),q=n("br"),M=s(' I use "https://wallpapercave.com/wp/wp13467289.png" as background in my individual character sites.')},l(e){l=o(e,"P",{"data-svelte-h":!0}),P(l)!=="svelte-tyo475"&&(l.textContent=T),u=U(e),d=o(e,"BR",{}),b=r(e,` Today I started by trying to link my "se more" buttons to diffrent sites depending on which character you click on. 
+`),h=o(e,"BR",{}),m=r(e,` I asked copilot and the AI said to use "$base/charakter" which is a reactive statement. It worked... 
+`),c=o(e,"BR",{}),f=r(e,` I have also worked on making my site more accesible for everyone. 
+`),p=o(e,"BR",{}),w=r(e,` I wanted to be able to change my characters outfits by clicking on a button with the name of the outfit on. 
+`),y=o(e,"BR",{}),I=r(e,` --> So I made a button that was called "warrior outfit" and then a funtion "warrior". And then with help of copilot I figuerd out how to make it possible to change the outfit.
+`),k=o(e,"BR",{}),v=r(e,` I used a currentOutfit and set it to the img and then in the function I set currentOutfit to a new outfit.
+`),g=o(e,"BR",{}),R=r(e,` I have made the information easier to read. 
+`),B=o(e,"BR",{}),x=r(e,` With media quiries I experiment on how to make it better when you use a phone. 
+`),S=o(e,"BR",{}),_=r(e,`I used "Master Media Queries And Responsive CSS Web Design Like a Chameleon!" youtube video by Slaying the dragon".
+`),C=o(e,"BR",{}),A=r(e,` --> To get a better understanding of how to use media queries.
+`),q=o(e,"BR",{}),M=r(e,' I use "https://wallpapercave.com/wp/wp13467289.png" as background in my individual character sites.')},m(e,t){i(e,l,t),i(e,u,t),i(e,d,t),i(e,b,t),i(e,h,t),i(e,m,t),i(e,c,t),i(e,f,t),i(e,p,t),i(e,w,t),i(e,y,t),i(e,I,t),i(e,k,t),i(e,v,t),i(e,g,t),i(e,R,t),i(e,B,t),i(e,x,t),i(e,S,t),i(e,_,t),i(e,C,t),i(e,A,t),i(e,q,t),i(e,M,t)},p:O,i:O,o:O,d(e){e&&(a(l),a(u),a(d),a(b),a(h),a(m),a(c),a(f),a(p),a(w),a(y),a(I),a(k),a(v),a(g),a(R),a(B),a(x),a(S),a(_),a(C),a(A),a(q),a(M))}}}const F={title:"Blogpost 2",date:"2025-05-07"};class G extends L{constructor(l){super(),Q(this,l,null,j,D,{})}}export{G as default,F as metadata};
---
+++ docs/_app/immutable/entry/start.BgrHpIvN.js
@@ -0,0 +1,1 @@
+import{b as a}from"../chunks/entry.CoEojGL_.js";export{a as start};
